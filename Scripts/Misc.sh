6d9ea8e6cb41def3d5b4c9738e8b0fc843b65714
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2021-04-27 11:54:09 +0200

    added misc function

---
+++ Scripts/Misc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# misc functions:
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# converting .bcf to .vcf format
+bcftools view <input.bcf> | bcftools view > <output.vcf>  # vcf requires much more space than binary bcf
+
+# grep'in NV2.10864 (twist) from .vcf file
+grep 'NV2.10864' <search.file.vcf>
+
