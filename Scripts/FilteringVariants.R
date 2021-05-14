b1ccca0af4d1be0fd8e836322f2e2c3ddc5a782c
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2021-05-14 12:19:55 +0200

    updated data

--- Scripts/FilteringVariants.R
+++ Scripts/FilteringVariants.R
@@ -1,6 +1,10 @@
 ## Filtering INDEL variants called by bcftools;
-## 
-## 05/11/2021:
+## Pre-defined filtering steps, where we believe that variants will not effect protein function
+## Concentrate on unique and multipe alternative genoypes respectively
+## Filtering rules, as commented below;
+##
+## Start: 05/11/2021:
+## Chris Kreitzer
 
 
 
@@ -11,17 +15,20 @@ setwd('~/Documents/GitHub/Twist/')
 
 ## Libraries
 library(stringr)
-
+library(tidyverse)
 
 ## Input
 chr2 = read.csv('Data_out/chr2_filtered_intersected.txt', sep = '\t', header = F)
-colnames(chr2)[1:5] = c('Chromsome', 'Position', 'ID', 'ref', 'alt')
 
 
-## Processing:
+
+## Processing
 FilterVariants = function(data.in){
+  columns.keep = c('Chromosome', 'Position', 'ref', 'alt', 'INS', 'DEL', 'delta', 'het', 'keep')
   data.in = as.data.frame(data.in)
-  colnames(data.in)[1:5] = c('Chromsome', 'Position', 'ID', 'ref', 'alt')
+  colnames(data.in)[1:5] = c('Chromosome', 'Position', 'ID', 'ref', 'alt')
+  annotation = grep('gene_id*', data.in)
+  colnames(data.in)[annotation] = 'Annotation'
   data.in$length = lengths(strsplit(data.in$alt, split = ',', fixed = T))
   
   
@@ -45,39 +52,85 @@ FilterVariants = function(data.in){
   data.single[c(ii.mismatch, ii.match), 'filter'] = 'keep'
   
   # filter variants where more than 2 samples are heterozygous or non-genotyped;
-  for(i in c(10:24)){
+  start.samples = grep('GT:PL', data.single) + 1
+  end.samples = start.samples + 15
+  
+  for(i in seq(start.samples, end.samples, 1)){
     data.single[, i] = substr(x = data.single[, i], start = 1, stop = 3)
   }
   
   data.single$het = NA
-  data.single$het = apply(data.single[10:24], 1, function(x) length(which(x == '0/1')))
+  data.single$het = apply(data.single[seq(start.samples, end.samples, 1)], 1, function(x) length(which(x == '0/1')))
   
   data.single$keep = ifelse(is.na(data.single$filter), 'discard',
                             ifelse(data.single$filter == 'keep' & data.single$het > 2, 'discard', 'keep'))
   
+  data.single = data.single[, colnames(data.single) %in% columns.keep]
   
-  ## double, or more alt alleles:
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # double, or more alternative genotypes
   data.double = data.in[which(data.in$length > 1), ]
-  data.double$INS = NA
-  data.double$DEL = NA
+  max_alternative_genotypes = max(data.in$length)
   
-  for(i in 1:nrow(data.double)){
-    data.double$INS[i] = any(grepl(pattern = data.double$ref[i], x = unlist(strsplit(data.double$alt[i], split = ',', fixed = T))))
-    data.double$DEL[i] = any(grepl(pattern = paste(unlist(strsplit(data.double$alt[i], split = ',', fixed = T)), collapse = '|'),
-                                   x = data.double$ref[i]))
+  data.multipe.genotypes = data.double %>%
+    separate(alt, c(paste0('Genotype', seq(1, max_alternative_genotypes, 1))), 
+             sep = ',',
+             remove = F)
+  
+  # determine whether Insertion or Deletion: is ref-pattern found within alternative alleles
+  # working on Insertions (ref shorter than alternative genotypes)
+  data.multipe.genotypes$INS = NA
+  
+  for(i in 1:nrow(data.multipe.genotypes)){
+    data.multipe.genotypes$INS[i] = any(grepl(pattern = data.multipe.genotypes$ref[i],
+                                              x = data.multipe.genotypes[i, c(grep(pattern = 'Genotype*', x = colnames(data.multipe.genotypes)))]))
   }
   
+  # working on deletions: alterative genotypes shorter than reference
+  data.multipe.genotypes$DEL = NA
+  genotypes = gsub(pattern = ',', replacement = '|', x = data.multipe.genotypes$alt)
+  data.multipe.genotypes$alt = genotypes
   
+  for(i in 1:nrow(data.multipe.genotypes)){
+    data.multipe.genotypes$DEL[i] = grepl(data.multipe.genotypes$alt[i], data.multipe.genotypes$ref[i])
+  }
   
-  data.double$delta = max(abs(nchar(example$ref) - nchar(unlist(strsplit(example$alt, split = ',')))))
-  data.double$keep = ifelse(data.double$match & data.double$delta > 3, 'keep', 'discard')
+  # determine INDEL extend
+  data.multipe.genotypes$delta = NA
   
-  # merge the two data frames
-  data.out = rbind(data.single, data.double)
+  for(i in 1:nrow(data.multipe.genotypes)){
+    data.multipe.genotypes$delta[i] = max(abs(nchar(data.multipe.genotypes$ref[i]) - 
+                                                      nchar(data.multipe.genotypes[i, c(grep(pattern = 'Genotype*', x = colnames(data.multipe.genotypes)))])), na.rm = T)
+  }
   
-  return(data.out)
+  ## filter variants where more than 2 samples are heterozygous or non-genotyped
+  start.samples = grep('GT:PL', data.multipe.genotypes) + 1
+  end.samples = start.samples + 15
+  
+  for(i in seq(start.samples, end.samples, 1)){
+    data.multipe.genotypes[, i] = substr(x = data.multipe.genotypes[, i], start = 1, stop = 3)
+  }
+  
+  data.multipe.genotypes$het = NA
+  data.multipe.genotypes$het = apply(data.multipe.genotypes[seq(start.samples, end.samples, 1)], 1, function(x) length(which(x == '0/1')))
+  
+  data.multipe.genotypes$keep = ifelse(data.multipe.genotypes$delta > 3 & data.multipe.genotypes$het < 3, 'keep', 'discard')
+  
+  data.multipe.genotypes = data.multipe.genotypes[, colnames(data.multipe.genotypes) %in% columns.keep]
+  
+  # combined output
+  Variants_out = rbind(data.single, data.multipe.genotypes)
+  Variants_keep = Variants_out[which(Variants_out$keep == 'keep'), ]
+  
+  Variants_annotated = merge(Variants_keep, data.in[, c('Position', 'Annotation')],
+                             by.x = 'Position', by.y = 'Position', all.x = T)
+  
+  Variants_annotated = unique(Variants_annotated)
+  
+  return(Variants_annotated)
   
 }
 
+x = FilterVariants(data.in = chr2)
 
-## Filtering heterozygous variants 
+write.table(x, file = 'Data_out/Filtered_Annotated_Variants_chr2.xlsx', sep = '\t', row.names = F)
